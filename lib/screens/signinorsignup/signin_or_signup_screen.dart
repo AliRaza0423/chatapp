53535ccc434078cb87906434bf6f6f1a22d71416
AliRaza0423 <89023546+AliRaza0423@users.noreply.github.com>
2021-08-31 17:24:18 +0500

    Added tab view

--- lib/screens/signinorsignup/signin_or_signup_screen.dart
+++ lib/screens/signinorsignup/signin_or_signup_screen.dart
@@ -3,40 +3,65 @@ import 'package:chatapp/constants.dart';
 import 'package:chatapp/screens/chats/chats_screen.dart';
 import 'package:flutter/material.dart';
 
-class SigninOrSignup extends StatelessWidget {
+class SigninOrSignup extends StatefulWidget {
+  @override
+  _SigninOrSignupState createState() => _SigninOrSignupState();
+}
+
+class _SigninOrSignupState extends State<SigninOrSignup>
+    with SingleTickerProviderStateMixin {
+  TabController _controller;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = TabController(length: 2, vsync: this, initialIndex: 0);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-          child: Column(
-            children: [
-              Spacer(flex: 2),
-              Image.asset(
-                MediaQuery.of(context).platformBrightness == Brightness.light
-                    ? 'assets/images/Logo_light.png'
-                    : 'assets/images/Logo_dark.png',
-                height: 146,
-              ),
-              Spacer(),
-              PrimaryButton(
-                text: 'Sign In',
-                press: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => ChatsScreen()));
-                },
-              ),
-              SizedBox(height: kDefaultPadding * 1.5),
-              PrimaryButton(
-                color: Theme.of(context).colorScheme.secondary,
-                text: 'Sign Up',
-                press: () {},
-              ),
-              Spacer(flex: 2),
-            ],
+    return Container(
+      color: Theme.of(context).scaffoldBackgroundColor,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.5,
+            child: AspectRatio(aspectRatio: 1.6,
+            child: Image(image: AssetImage(MediaQuery.of(context).platformBrightness == Brightness.light
+                  ? 'assets/images/Logo_light.png'
+                  : 'assets/images/Logo_dark.png',
+            )),),
           ),
-        ),
+          Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: AppBar(
+              automaticallyImplyLeading: false,
+              backgroundColor: Colors.transparent,
+              bottom: TabBar(
+                controller: _controller,
+                labelColor: MediaQuery.of(context).platformBrightness == Brightness.light 
+                ? kContentColorLightTheme
+                : kContentColorDarkTheme ,
+                tabs: [
+                  Tab(
+                    text: 'Log in'
+                  ),
+                  Tab(
+                    text: 'Sign Up',
+                  )
+                ],
+              ),
+            ),
+            body: TabBarView(
+              controller: _controller,
+              children: [
+                Text('1'),
+                Text('1'),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
